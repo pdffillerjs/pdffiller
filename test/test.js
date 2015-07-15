52af7fef1b21ebfbb3cc39447e0b195b3e17e113
David Baldwynn <whitef0x0@users.noreply.github.com>
2015-07-15 12:54:54 -0700

    Added test for convFieldJson2FDF

--- test/test.js
+++ test/test.js
@@ -13,7 +13,7 @@ var pdfFiller = require( 'pdffiller' ),
 
 var sourcePDF,
     destinationPDF,
-    data, expected;
+    data, expected, convMap;
 
 /**
  * Unit tests
@@ -34,8 +34,8 @@ describe('fillForm Unit Tests', function(){
         sourcePDF = "test/test.pdf";
     });
     
-    pdfFiller.generateFieldJson( sourcePDF, function(form_fields) { 
-        form_fields.should.be.equal(expected);
+    pdfFiller.fillForm( sourcePDF, destinationPDF, data, function(err) { 
+        should.not.exist(err);
         done();
     });
 });
@@ -45,46 +45,188 @@ describe('generateFieldJson Unit Tests', function(){
         expected = [
             {
                 "title" : "last_name",
-                "type": "Text"
+                "fieldType": "Text"
             },
             {
                 "title" : "first_name",
-                "type": "Text"
+                "fieldType": "Text"
             },
             {
                 "title" : "date",
-                "type": "Text"
+                "fieldType": "Text"
             },
             {
                 "title" : "football",
-                "type": "Button"
+                "fieldType": "Button"
             },
             {
                 "title" : "baseball",
-                "type": "Button"
+                "fieldType": "Button"
             },
             {
                 "title" : "basketball",
-                "type": "Button"
+                "fieldType": "Button"
             },
             {
                 "title" : "hockey",
-                "type": "Button"
+                "fieldType": "Button"
             },
             {
                 "title" : "nascar",
-                "type": "Button"
+                "fieldType": "Button"
             }
         ];
         sourcePDF = "test/test.pdf";
     });
     it('Should generate form field JSON as expected', function(){
-        pdfFiller.fillForm( sourcePDF, function(form_fields) { 
+        pdfFiller.generateFieldJson( sourcePDF, function(err, form_fields) { 
+            should.not.exist(err);
             form_fields.should.equal(expected);
             done();
         });
     });
+    
 });
+
+describe('generateFDFTemplate Unit Tests', function(){
+    beforeEach(function(done){
+        expected = {
+            "last_name" : "",
+            "first_name" : "",
+            "date" : "",
+            "football" : "",
+            "baseball" : "",
+            "basketball" : "",
+            "hockey" : "",
+            "nascar" : ""
+        };
+        sourcePDF = "test/test.pdf";
+    });
+    it('Should generate an empty FDF Template as expected', function(){
+        pdfFiller.generateFDFTemplate( sourcePDF, function(err, fdfTemplate) { 
+            should.not.exist(err);
+            fdfTemplate.should.equal(expected);
+            done();
+        });
+    });
+    
+});
+
+describe('convFieldJson2FDF Unit Tests', function(){
+    beforeEach(function(done){
+        expected = {
+            "last_name" : "John",
+            "first_name" : "Doe",
+            "date" : "Jan 1, 2013",
+            "football" : "Off",
+            "baseball" : "Yes",
+            "basketball" : "Off",
+            "hockey" : "Yes",
+            "nascar" : "Off"
+        };
+        
+        data = [
+            {
+                "title" : "last_name",
+                "fieldfieldType": "Text",
+                "fieldValue": "Doe"
+            },
+            {
+                "title" : "first_name",
+                "fieldfieldType": "Text",
+                "fieldValue": "John"
+            },
+            {
+                "title" : "date",
+                "fieldType": "Text",
+                "fieldValue": "Jan 1, 2013"
+            },
+            {
+                "title" : "football",
+                "fieldType": "Button",
+                "fieldValue": false
+            },
+            {
+                "title" : "baseball",
+                "fieldType": "Button",
+                "fieldValue": true
+            },
+            {
+                "title" : "basketball",
+                "fieldType": "Button"
+                "fieldValue": false
+            },
+            {
+                "title" : "hockey",
+                "fieldType": "Button"
+                "fieldValue": true
+            },
+            {
+                "title" : "nascar",
+                "fieldType": "Button"
+                "fieldValue": false
+            }
+        ];
+        sourcePDF = "test/test.pdf";
+    });
+    it('Should generate an empty FDF Template as expected', function(){
+        var FDFData = pdfFiller.convFieldJson2FDF( data );
+        FDFData.should.equal(expected);
+        done();
+    });
+    
+});
+
+describe('mapForm2PDF Unit Tests', function(){
+    beforeEach(function(done){
+        var convMap = {
+            "lastName": "last_name",
+            "firstName": "first_name",
+            "Date": "date",
+            "lastName": "last_name",
+            "footballField": "football",
+            "bballField": "basketball",
+            "baseballField": "baseball",
+            "hockeyField": "hockey",
+            "nascarField": "nascar"
+        };
+        
+        data = {
+            "lastName" : "John",
+            "firstName" : "Doe",
+            "Date" : "Jan 1, 2013",
+            "footballField" : "Off",
+            "baseballField" : "Yes",
+            "bballField" : "Off",
+            "hockeyField" : "Yes",
+            "nascarField" : "Off"
+        };
+        
+        expected = {
+            "last_name" : "John",
+            "first_name" : "Doe",
+            "date" : "Jan 1, 2013",
+            "football" : "Off",
+            "baseball" : "Yes",
+            "basketball" : "Off",
+            "hockey" : "Yes",
+            "nascar" : "Off"
+        };
+        sourcePDF = "test/test.pdf";
+    });
+    it('Should generate form field JSON as expected', function(){
+        pdfFiller.mapForm2PDF( data, convMap, function(err, mappedFields) { 
+            should.not.exist(err);
+            mappedFields.should.equal(expected);
+            done();
+        });
+    });
+    
+});
+
+
+
+
 
 
 
