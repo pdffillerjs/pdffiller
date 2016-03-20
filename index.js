ccf829ae7d7dd26ffa3b9a40b049336ecff7a4e6
JaakkoL <jaakko.o.laurila@gmail.com>
2016-03-20 16:48:10 +0100

    Use utf8-fdf-generator instead of fdf as a dependency.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
 (function(){
     var child_process = require('child_process'),
         exec = require('child_process').exec,
-        fdf = require('fdf'),
+        fdf = require('utf8-fdf-generator'),
         _ = require('lodash'),
         fs = require('fs');
 
@@ -23,7 +23,7 @@
                 } catch(err){
 
                     return key;
-                } 
+                }
                 return convMap[key];
             });
 
@@ -54,7 +54,7 @@
                 regFlags = /FieldFlags: ([0-9\t .]+)/,
                 fieldArray = [],
                 currField = {};
-            
+
             if(nameRegex !== null && (typeof nameRegex) == 'object' ) regName = nameRegex;
 
             exec( "pdftk " + sourceFile + " dump_data_fields_utf8 " , function (error, stdout, stderr) {
@@ -62,34 +62,34 @@
                     console.log('exec error: ' + error);
                     return callback(error, null);
                 }
-                
+
                 fields = stdout.toString().split("---").slice(1);
                 fields.forEach(function(field){
                     currField = {};
-                    
-                    currField['title'] = field.match(regName)[1].trim() || ''; 
+
+                    currField['title'] = field.match(regName)[1].trim() || '';
 
                     if(field.match(regType)){
-                        currField['fieldType'] = field.match(regType)[1].trim() || '';  
+                        currField['fieldType'] = field.match(regType)[1].trim() || '';
                     }else {
                         currField['fieldType'] = '';
                     }
 
                     if(field.match(regFlags)){
                         currField['fieldFlags'] = field.match(regFlags)[1].trim()|| '';
-                    }else{ 
+                    }else{
                         currField['fieldFlags'] = '';
                     }
 
                     currField['fieldValue'] = '';
-                    
+
                     fieldArray.push(currField);
                 });
-                
+
                 return callback(null, fieldArray);
             });
         },
-        
+
         generateFDFTemplate: function( sourceFile, nameRegex, callback ){
             this.generateFieldJson(sourceFile, nameRegex, function(err, _form_fields){
                 if (err) {
@@ -97,44 +97,36 @@
                   return callback(err, null);
                 }
                 var _keys   = _.pluck(_form_fields, 'title'),
-            	    _values = _.pluck(_form_fields, 'fieldValue'),
+                  _values = _.pluck(_form_fields, 'fieldValue'),
                     jsonObj = _.zipObject(_keys, _values);
-                
+
                 return callback(null, jsonObj);
-                
+
             });
         },
 
         fillForm: function( sourceFile, destinationFile, fieldValues, callback ) {
 
             //Generate the data from the field values.
-            var formData = fdf.generate( fieldValues ),
-                tempFDF = "data" + (new Date().getTime()) + ".fdf";
+            var tempFDF = "data" + (new Date().getTime()) + ".fdf",
+                formData = fdf.generator( fieldValues, tempFDF );
 
-            //Write the temp fdf file.
-            fs.writeFile( tempFDF, formData, function( err ) {
+            child_process.exec( "pdftk " + sourceFile + " fill_form " + tempFDF + " output " + destinationFile + " flatten", function (error, stdout, stderr) {
 
-                if ( err ) {
-                    return callback(err);
+                if ( error ) {
+                    console.log('exec error: ' + error);
+                    return callback(error);
                 }
+                //Delete the temporary fdf file.
+                fs.unlink( tempFDF, function( err ) {
 
-                child_process.exec( "pdftk " + sourceFile + " fill_form " + tempFDF + " output " + destinationFile + " flatten", function (error, stdout, stderr) {
-
-                    if ( error ) {
-                      console.log('exec error: ' + error);
-                      return callback(error);
+                    if ( err ) {
+                        return callback(err);
                     }
-                    //Delete the temporary fdf file.
-                    fs.unlink( tempFDF, function( err ) {
-
-                        if ( err ) {
-                            return callback(err);
-                        }
-                        // console.log( 'Sucessfully deleted temp file ' + tempFDF );
-                        return callback();
-                    });
-                } );
-            });
+                    // console.log( 'Sucessfully deleted temp file ' + tempFDF );
+                    return callback();
+                });
+            } );
         }
     };
 
